5eb85e2aec14b7ee096644b186a9776def76626e
Yaros <thedj.launchpadder.dmx512@gmail.com>
2021-06-09 19:55:50 +0200

    Create pythonpad.py

---
+++ pythonpad.py
@@ -0,0 +1,59 @@
+import os
+
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
+import pygame.midi
+from pygame.locals import *
+from win32 import *
+
+pygame.init()
+pygame.fastevent.init()
+event_get = pygame.fastevent.get
+event_post = pygame.fastevent.post
+
+pygame.midi.init()
+
+
+def scan_launchpads():
+    pygame.midi.init()
+    inputs = []
+    outputs = []
+    for i in range(pygame.midi.get_count()):
+        r = pygame.midi.get_device_info(i)
+        (interf, name, input, output, opened) = r
+        if "Launchpad" in str(name):
+
+            if input:
+                inputs.append([str(name)[2:][:-1], i])
+
+            if output:
+                outputs.append([str(name)[2:][:-1], i])
+
+        else:
+            pass
+    pygame.midi.quit()
+    return inputs, outputs
+
+
+def connect(connecttype, input_ch=0, output_ch=0):
+    global input_device
+    global output_device
+
+    if connecttype == "manual":
+        input_device = pygame.midi.Input(scan_launchpads()[0][input_ch][1])
+        output_device = pygame.midi.Output(scan_launchpads()[1][output_ch][1])
+
+    else:
+        input_device = pygame.midi.Input(scan_launchpads()[0][0][1])
+        output_device = pygame.midi.Output(scan_launchpads()[1][0][1])
+
+
+def detect_press():
+    try:
+        if input_device.poll():
+            midi_events = input_device.read(10)[0][0]
+
+            return [midi_events[1], midi_events[2]]
+
+    except:
+        raise IndexError("Device not detected or not connected properly. Use pythonpad.connect() first")
